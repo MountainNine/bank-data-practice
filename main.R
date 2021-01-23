24b0b1dfaff079584a6fdafde78ecb366db56885
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-24 00:02:48 +0900

    연체 관련 점수 계산 함수 작성 완료 및 결과 데이터프레임에 병합

--- main.R
+++ main.R
@@ -9,26 +9,27 @@ df_cdopn <- read.csv("./data/CDOPN.csv")
 
 df_id$age <- as.integer((2018 - df_id$BTH_YR) / 10)
 
-get_delay_score <- function(end_ym, dlq_amt, delay_cnt) {
-  result_score <- -100
-  current_date <- as.Date('201812', format = '%Y%m')
-  if (delay_cnt < 4) {
-    result_score <- result_score + (-100 * delay_cnt + (-0.001 * dlq_amt))
-  } else {
-    result_score <- result_score + (-400 +
-      (-10 * (delay_cnt - 4)) +
-      (-0.001 * dlq_amt))
-  }
+get_month_diff <- function(datetime_1, datetime_2) {
+  return((year(datetime_1) * 12 + month(datetime_1)) - (year(datetime_2) * 12 + month(datetime_2)))
+}
 
-  if (end_ym < current_date & delay_cnt < 4) {
-    result_score <- result_score +
-      100 +
-      difftime(end_ym, current_date) / 30 * 10
-  } else if (end_ym < as.Date("2018-12-31") & delay_cnt >= 4) {
-    result_score <- result_score +
-      10 +
-      difftime(end_ym, current_date) / 30 * 10
-  }
+get_delay_score <- function(delay_new) {
+  result_score <- -100
+  current_date <- as.Date('20181201', format = '%Y%m%d')
+  dlq_amt <- delay_new$DLQ_AMT
+  end_ym <- delay_new$YM
+  delay_cnt <- delay_new$delay_cnt
+  result_score <- ifelse(delay_cnt < 4, result_score + (-100 * delay_cnt + (-0.001 * dlq_amt)),
+                         result_score + (-400 +
+                           (-10 * (delay_cnt - 4)) +
+                           (-0.001 * dlq_amt)))
+
+  diff_month <- get_month_diff(current_date, end_ym)
+
+  result_score <- ifelse(end_ym < current_date,
+                         ifelse(delay_cnt < 4, result_score + 100 + diff_month * 10, result_score + 10 + diff_month * 10),
+                         result_score)
+
   return(result_score)
 }
 
@@ -37,29 +38,33 @@ get_delay_score <- function(end_ym, dlq_amt, delay_cnt) {
 ## 연체 진행 일수 경과(월에 -100/-10)
 ## 연체 해제(+100)
 ## 연체 해제 일수 경과(월에 +10)
-}
 
 ## 신용거래 경과 기간
 
 count_cdopn <- count(df_cdopn, JOIN_KEY, COM_KEY, CD_OPN_YM)
-df_cdopn_new <- count_cdopn %>% group_by(JOIN_KEY) %>% summarize(cd_cnt = min(CD_OPN_YM))
-df_result <- merge(df_id, df_cdopn_new, by="JOIN_KEY", all.x = TRUE)
+df_cdopn_new <- count_cdopn %>%
+  group_by(JOIN_KEY) %>%
+  summarize(cd_cnt = min(CD_OPN_YM))
+df_result <- merge(df_id, df_cdopn_new, by = "JOIN_KEY", all.x = TRUE)
 df_result$cd_cnt <- as.integer(2019 - as.integer(df_result$cd_cnt / 100))
 rm("df_cdopn_new", "count_cdopn")
 
 ## 장기, 단기 연체 발생 여부
 
-df_delay_new <- df_delay %>% group_by(JOIN_KEY, COM_KEY, DLQ_CD_1, DLQ_YM, DLQ_AMT) %>%
-summarise(YM = max(YM), delay_cnt = n())
-df_delay_final <- count(df_delay_new, JOIN_KEY)
-df_delay_final <- rename(df_delay_final, "delay_cnt"="n")
-df_result <- merge(df_result, df_delay, by="")
-if(df_delay_new$n<3){
-df_result$dlq_cnt <- 1
-} else if(df_delay_new$n>=3){
-df_result$dlq_cnt <- 2
-}
+df_delay_new <- df_delay %>%
+  group_by(JOIN_KEY, COM_KEY, DLQ_CD_1, DLQ_YM, DLQ_AMT) %>%
+  summarise(YM = max(YM))
+df_delay_new$YM <- paste0(as.character(df_delay_new$YM), "01")
+df_delay_new$YM <- as.Date(df_delay_new$YM, format = "%Y%m%d")
+df_delay_new$DLQ_YM <- paste0(as.character(df_delay_new$DLQ_YM), "01")
+df_delay_new$DLQ_YM <- as.Date(df_delay_new$DLQ_YM, format = "%Y%m%d")
+df_delay_new$delay_cnt <- get_month_diff(df_delay_new$YM, df_delay_new$DLQ_YM) + 1
 
-if(is.na(df_result$dlq_cnt)){
-df_result$dlq_cnt<-0
-}
+df_delay_new$dlq_score <- get_delay_score(df_delay_new)
+df_delay_final <- df_delay_new %>%
+  group_by(JOIN_KEY) %>%
+  summarise(dlq_score = sum(dlq_score))
+df_result <- merge(df_result, df_delay_final, by = "JOIN_KEY", all.x = TRUE)
+df_result[is.na(df_result$dlq_score),]$dlq_score <- 0
+
+rm("df_delay_new", "df_delay_final")
