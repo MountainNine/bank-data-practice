51ecc02e9691e7bd28d8b29366a31cbd71b23f15
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-24 15:14:07 +0900

    대출 발생, 상환 여부 코드 작성 중

--- main.R
+++ main.R
@@ -37,6 +37,14 @@ get_delay_score <- function(delay_new) {
   return(result_score)
 }
 
+get_loan_score <- function(df_loan) {
+  result_score <- -10
+  current_date <- as.Date('20181201', format = "%Y%m%d")
+  ln_amt <- df_loan$LN_AMT
+  end_ym <- df_loan$YM
+
+}
+
 ## 장기, 단기 연체 발생 여부
 
 df_delay_new <- df_delay %>%
@@ -46,6 +54,7 @@ df_delay_new$YM <- paste0(as.character(df_delay_new$YM), "01")
 df_delay_new$YM <- as.Date(df_delay_new$YM, format = "%Y%m%d")
 df_delay_new$DLQ_YM <- paste0(as.character(df_delay_new$DLQ_YM), "01")
 df_delay_new$DLQ_YM <- as.Date(df_delay_new$DLQ_YM, format = "%Y%m%d")
+
 df_delay_new$delay_cnt <- get_month_diff(df_delay_new$YM, df_delay_new$DLQ_YM) + 1
 df_delay_new$long_delay <- is_long_delay(df_delay_new)
 df_delay_new$dlq_score <- get_delay_score(df_delay_new)
@@ -56,7 +65,7 @@ df_result <- merge(df_id, df_delay_final, by = "JOIN_KEY", all.x = TRUE)
 df_result[is.na(df_result$dlq_score),]$dlq_score <- 0
 df_result[is.na(df_result$long_delay),]$long_delay <- 0
 
-rm("df_delay_new", "df_delay_final")
+rm("df_delay_final")
 
 ## 신용거래기간 경과 여부
 
@@ -70,5 +79,16 @@ df_result$cd_score <- ifelse(df_result$long_delay, df_result$cd_score*10, df_res
 df_result[is.na(df_result$cd_score),]$cd_score <- 0
 rm("df_cdopn_new", "count_cdopn")
 
-
 ## 대출 발생, 상환 여부
+df_loan_new <- df_loan %>% group_by(JOIN_KEY, COM_KEY, SCTR_CD, LN_CD_1, LN_CD_2, LN_YM, LN_AMT) %>%
+  summarise(cnt= n(), YM = max(YM))
+df_loan_delay <- merge(df_loan_new, df_delay_new, by=c("JOIN_KEY", "COM_KEY"), all.x = TRUE)
+df_loan_delay$YM.x <- paste0(as.character(df_loan_delay$YM.x), "01")
+df_loan_delay$YM.x <- as.Date(df_loan_delay$YM.x, format = "%Y%m%d")
+df_loan_delay$LN_YM <- paste0(as.character(df_loan_delay$LN_YM), "01")
+df_loan_delay$LN_YM <- as.Date(df_loan_delay$LN_YM, format = "%Y%m%d")
+df_loan_delay$ln_cnt <- get_month_diff(df_loan_delay$YM.x,df_loan_delay$LN_YM) + 1
+df_loan_second <- merge(df_loan_new, df_result[,c("JOIN_KEY","long_delay")],
+                        by="JOIN_KEY", all.x = TRUE)
+rm(df_loan_new)
+df_loan_score <- get_loan_score(df_loan_second)
