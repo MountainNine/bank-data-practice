ddb1541f43a7f5515f054c6e1c55b7c769ff78ff
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-26 09:33:05 +0900

    library, data import

--- analysis_banking.R
+++ analysis_banking.R
@@ -1,3 +1,11 @@
+library(dplyr)
+library(ggplot2)
+
+df_id <- read.csv("./data/ID.csv")
+df_loan <- read.csv("./data/LN.csv")
+df_delay <- read.csv("./data/DLQ.csv")
+df_cdopn <- read.csv("./data/CDOPN.csv")
+
 # 1. 연령대별 주요 대출상품 교차분석
 # 2. 연체금액 -> 연체횟수에 대한 상관관계
 # 3. 카드개설 수 -> 대출 금액의 회귀분석
